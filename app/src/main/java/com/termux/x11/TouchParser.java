a27315948785d726511effd375e86985b18173de
hffariel <farielclaire@gmail.com>
2022-10-26 01:39:34 +0800

    Update TouchParser.java

--- app/src/main/java/com/termux/x11/TouchParser.java
+++ app/src/main/java/com/termux/x11/TouchParser.java
@@ -533,7 +533,7 @@ public class TouchParser {
             mListener.onPointerMotion((int) e.getX(), (int) e.getY());
 
             currentBS = e.getButtonState();
-            if (e.getSource() == 12290 && (e.getAction() == 0 || (e.getAction() == 2 && ev.getPointerCount() > 1))) {
+            if (e.getSource() == 12290 && (e.getAction() == 0 || (e.getAction() == 2 && ev.getPointerCount() <= 1))) {
                 currentBS = 1;
             }
             if (isMouseButtonChanged(MotionEvent.BUTTON_PRIMARY)) {
