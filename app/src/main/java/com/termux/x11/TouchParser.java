d2fbd56be1015c561d5b68880d77cf1d1a6c4c55
hffariel <farielclaire@gmail.com>
2022-10-26 01:43:31 +0800

    Update TouchParser.java

--- app/src/main/java/com/termux/x11/TouchParser.java
+++ app/src/main/java/com/termux/x11/TouchParser.java
@@ -206,7 +206,7 @@ public class TouchParser {
     }
 
     boolean onTouchEvent(MotionEvent ev) {
-        Toast.makeText(target.getRootView().findViewById(android.R.id.content).getContext(), "pcount:"+ev.getPointerCount()+ " pointer: "+ev.getPointerId()+ " mask: "+ev.getActionMasked()+ " action: "+ev.getAction()+ " buttonstate: "+ev.getButtonState(), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(target.getRootView().findViewById(android.R.id.content).getContext(), "pcount:"+ev.getPointerCount()+ " mask: "+ev.getActionMasked()+ " action: "+ev.getAction()+ " buttonstate: "+ev.getButtonState(), Toast.LENGTH_SHORT).show();
         if ((ev.getSource() & InputDevice.SOURCE_MOUSE) == InputDevice.SOURCE_MOUSE || ev.getSource() == 12290) {
             return hmListener.onTouch(ev);
         }
